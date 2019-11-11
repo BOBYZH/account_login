276c8de695d1e4fca823e1c8baef6512e7b5ea69
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-11-11 16:24:36 +0000

    Update login and logout function with session package

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Account Login v1.0 (登入功能模擬，1.0版)
+# Account Login v1.1 (登入功能模擬，1.1版)
 
 ALPHA Camp 學期三作業3、4: 登入功能
 
 ### 版本歷史：
+- v1.1 完成儲存「登入狀態」(使用Cookie驗證權限，含建立、維持與取消)
 - v1.0 完成基本功能(模擬登入與登出)
 
 ## 如何使用：
@@ -11,7 +12,7 @@ ALPHA Camp 學期三作業3、4: 登入功能
 2. 使用終端機(terminal)，將目錄切換至專案資料夾(account_Login)
 3. 使用npm安裝需要的套件：
 ```
-npm i express express-handlebars
+npm i express express-handlebars express-session
 ```
 4. 執行本專案：
 ```
@@ -35,6 +36,7 @@ npm run start
 ######
 2. 如果輸入的帳密如以上五組正確，會進入登入成功的歡迎畫面："Welcome back, (firstName)!"，右側則有"logout"選項可登出
 3. 如果帳密組合錯誤，會停留在本頁並跳出錯誤訊息："invalid email or password"，其中email會留在欄位方便改用其他密碼；另外，點標題"Account Login"可消除錯誤訊息
+4. (1.1新增)登入後會以session建立權限，右上角多一個"Home"與"Logout"，前者維持同一畫面，表現維持登入狀態；後者則會取消登入狀態，得回首頁重新登入
 
 ## 預覽畫面：
 ![image](https://assets-lighthouse.s3.amazonaws.com/uploads/answer/cover/107006/account_login.png)
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const loginCheck = require('./loginCheck')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -9,8 +10,18 @@ app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extended: true }))
 
+app.use(session({
+  secret: 'avengers assemble',
+  resave: true,
+  saveUninitialized: false
+}))
+
 app.get('/', (req, res) => {
-  res.render('index')
+  if (req.session.userName) {
+    res.render('welcome', { userName: req.session.userName })
+  } else {
+    res.render('index')
+  }
 })
 
 app.post('/', (req, res) => {
@@ -22,8 +33,15 @@ app.post('/', (req, res) => {
     res.render('index', { errorMessage, inputInfo })
   } else {
     const userName = searchResult.firstName
+    req.session.userName = userName
+    console.log(req.session.id)
     res.render('welcome', { userName })
   }
+})
+
+app.get('/logout', function (req, res) {
+  req.session.destroy()
+  res.redirect('/')
 })
 
 app.listen(port, () => {
